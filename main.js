4e77003cb0dcd35baaeab92f8193cf559239980d
reddelijah18 <102178506+reddelijah18@users.noreply.github.com>
2022-07-19 11:33:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css';
+
+const nameInput = document.querySelector('input');
+
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if (nameInput.value === '') {
+    nameInput.setCustomValidity('Enter your name!');
+  } else {
+    nameInput.setCustomValidity('Names can only contain upper and lowercase letters. Try again!');
+  }
+});
